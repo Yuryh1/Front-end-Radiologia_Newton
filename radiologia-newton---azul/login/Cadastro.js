cb8c713fc44a57c63d8d711d30aa006a9cb84a13
Yury Samuel <Yuryh084@gmail.com>
2023-12-02 02:17:55 -0300

    Update Cadastro.js

--- radiologia-newton---azul/login/Cadastro.js
+++ radiologia-newton---azul/login/Cadastro.js
@@ -10,7 +10,7 @@ import * as AuthApi from "./AuthApi";
 export default function RegisterScreen() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
-  const [ra, setRa] = useState('');
+  const [RA, setRA] = useState('');
   const [password, setPassword] = useState('');
 
   const [image, setImage] = useState(null);
@@ -45,54 +45,99 @@ const pickImage = async () => {
   )}
 </TouchableOpacity>
 
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', padding: 16 }}>
-      <Image
-        style={styles.logo}
-        source={Logo}
-      />
-      <TextInput
-        label="Nome"
-        value={name}
-        onChangeText={text => setName(text)}
-      />
-      <TextInput
-        label="Email"
-        value={email}
-        onChangeText={text => setEmail(text)}
-      />
-      <TextInput
-        label="RA"
-        value={ra}
-        onChangeText={text => setRa(text)}
-      />
-      <TextInput
-        label="Senha"
-        value={password}
-        onChangeText={text => setPassword(text)}
-        secureTextEntry
-      />
-      <Button mode="contained" onPress={() => handleSubmit()}>
-        Cadastrar
-      </Button>
+    return (
+    <View style={styles.container}>
+      <View style={styles.img}>
+        <Image source={Logo} style={styles.logo} />
+      </View>
+      <View style={styles.inputs}>
+        <TextInput
+          value={name}
+          onChangeText={(text) => setname(text)}
+          style={styles.name}
+          label="Nome"
+          mode="flat"
+          theme={{
+            colors: {
+              primary: "black",
+            },
+          }}
+        />
+        <TextInput
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          style={styles.email}
+          label="E-mail"
+          mode="flat"
+          theme={{
+            colors: {
+              primary: "black",
+            },
+          }}
+        />
+        <TextInput
+          value={RA}
+          onChangeText={(text) => setRA(text)}
+          style={styles.RA}
+          label="RA"
+          mode="flat"
+          theme={{
+            colors: {
+              primary: "black",
+            },
+          }}
+        />
+        <TextInput
+          value={password}
+          onChangeText={(text) => setpassword(text)}
+          style={styles.password}
+          label="password"
+          mode="password"
+          theme={{
+            colors: {
+              primary: "black",
+            },
+          }}
+        />
+
+        <Button
+          mode="contained"
+          buttonColor="#193073"
+          textColor="white"
+          style={styles.botao}
+          onPress={() => {
+            login("email");
+          }}
+        >
+          Cadastrar
+        </Button>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#27A4F2",
+    height: "100%",
+    justifyContent: "center",
+  },
   logo: {
-    marginTop:150,
+    marginTop: 40,
     width: 200,
     height: 200,
-    alignSelf: "center"
+    alignSelf: "center",
   },
-   profileImageButton: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ccc',
+  inputs: {
+    padding: 8,
+  },
+  
+  botao: {
     marginTop: 10,
+    width: 200,
+    height: 30,
+    borderRadius: 10,
+    alignSelf: "center",
+    justifyContent: "center",
   }
 });
